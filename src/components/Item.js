e369410fca64c7ad2f0cd1189903ab268b7621d9
Tarik1112 <tare.mujke@gmail.com>
2021-09-20 13:15:41 +0200

    Adding a form to the app

--- src/components/Item.js
+++ src/components/Item.js
@@ -1,18 +1,25 @@
+import React, { useState } from "react";
 import "./Item.css";
 import ItemDate from "./ItemDate";
-import Card from "./UI/Card"
+import Card from "./UI/Card";
 function Item(props) {
+  const [title, setTitle] = useState(props.title);
 
+  const changeTitle = () => {
+    setTitle("Apple");
+    console.log(title);
+  };
 
   return (
     <Card className="item">
       <ItemDate date={props.date}></ItemDate>
       <div className="item-description">
-        <h3>{props.title}</h3>
+        <h3>{title}</h3>
 
         <div className="item-price">
           <span>${props.amount}</span>
         </div>
+        <button onClick={changeTitle}>Change title!</button>
       </div>
     </Card>
   );
